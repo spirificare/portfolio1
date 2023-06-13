c5b222895650416f067ee6807fef4bf9feb73c9f
Agne Askelianec <11472629-spirificare@users.noreply.gitlab.com>
2023-06-13 15:04:05 +0300

    improve layout

--- js/script.js
+++ js/script.js
@@ -1,29 +1,19 @@
-"use strict";
+'use strict';
 
 const menuBurger = document.querySelector('.header__burger');
 const headerMenu = document.querySelector('.header__menu');
 
 if (menuBurger && headerMenu) {
-   menuBurger.addEventListener("click", function (e) {
+   menuBurger.addEventListener('click', function (e) {
       document.body.classList.toggle('_lock');
       menuBurger.classList.toggle('_active');
       headerMenu.classList.toggle('_active');
    })
 }
-   
-window.addEventListener("scroll", () => {
+
+window.addEventListener('scroll', () => {
    const headerElement = document.querySelector('.header');
-   if (window.scrollY <= 0) {
-      headerElement.classList.remove('_scroll');
-   } else {
-      headerElement.classList.add('_scroll');
+   if (headerElement) {
+      headerElement.classList.toggle('_scroll', window.scrollY > 0);
    }
 });
-
-/* 
-window.addEventListener("scroll", () => {
-   const headerElement = document.querySelector('.header');
-   headerElement.classList.toogle('_scroll', window.scrollY <= 0);
-});
-
-*/
